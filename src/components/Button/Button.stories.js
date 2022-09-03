95fbd01a0e0701806bddbacb4bdddc3f8408ec59
vipulpathak113 <vipulpathak113@gmail.com>
2022-09-03 20:27:43 +0530

    stories using args

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,7 +3,7 @@ import React from "react";
 import Button from "./Button";
 
 export default {
-  title: "Button",
+  title: "Form/Button",
   component: Button,
 };
 
@@ -14,3 +14,26 @@ export const Secondary = () => <Button variant="secondary">Secondary</Button>;
 export const Success = () => <Button variant="success">Success</Button>;
 
 export const Danger = () => <Button variant="danger">Danger</Button>;
+
+const Template = (args) => <Button {...args} />;
+
+export const PrimaryArgs = Template.bind({});
+
+PrimaryArgs.args = {
+  variant: "primary",
+  children: "Primary Args",
+};
+
+export const SecondaryArgs = Template.bind({});
+
+SecondaryArgs.args = {
+  variant: "secondary",
+  children: "Secondary Args",
+};
+
+export const PrimaryLong = Template.bind({});
+
+PrimaryLong.args = {
+  ...PrimaryArgs,
+  children: "Primary Long Args",
+};
