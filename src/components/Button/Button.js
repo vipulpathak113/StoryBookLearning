1247cee4a48d3009cc4ac8af54367a49bd25c497
vipulpathak113 <vipulpathak113@gmail.com>
2022-09-03 14:04:02 +0530

    button story

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Button.css'
+
+export default function Button(props) {
+  const { variant = "primary", children, ...rest } = props;
+
+  return (
+    <button className={`button ${variant}`} {...rest}>
+      {children}
+    </button>
+  );
+}
